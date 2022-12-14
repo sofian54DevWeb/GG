3b96d79518354c543b607f077fd5e03d205d19d0
Sofian54DevWeb <sofian54500@gmail.com>
2022-12-14 15:13:44 +0100

    Ajout du plateau

--- Board.cs
+++ Board.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace GooseGame
+{
+    public class Board //Plateau de jeu avec les cases
+    {
+        public int Cas { get; set; }
+
+        public Board(int cas)
+        {
+            Cas = cas;
+        }
+
+    }
+}
